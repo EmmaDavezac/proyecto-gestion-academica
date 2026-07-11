b7dee4efa9e721f5caad9d7ebe8f5b60cc160baf
Emmanuel Davezac <lucianodavezac@gmail.com>
2026-07-11 17:58:39 -0300

    clase 5 terminada

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
+
+
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +9,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideRouter(routes)
   ]
 };
